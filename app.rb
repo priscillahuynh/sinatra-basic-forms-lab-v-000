3e89cea1c69789c30bb9dcfdd99bdcd581f2e6af
Priscilla H <priscillamhuynh@gmail.com>
2019-02-16 05:27:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  attr_accessor :name, :breed, :age 
+  @@all =[]
   
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age 
+    
 end
