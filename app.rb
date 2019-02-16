428be27367cd88335ed6718c23d10c91238f8faf
Priscilla H <priscillamhuynh@gmail.com>
2019-02-16 05:46:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
     erb :create_puppy
   end
   
-  post '/puppy' do 
+  post '/' do 
     @puppy = Puppy.new(params[:name], params[:breed], params[:age])
     erb :display_puppy
   end
